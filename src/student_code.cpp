710a1274aad70b63a417f795ee706d00c2b4752a
Shaban Satti <shabansatti02@gmail.com>
2026-02-18 11:48:29 +0500

    Update comment to correct character value from 'a' to 'A'

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -13,7 +13,7 @@ float getFloatValue() {
 }
 
 char getCharValue() {
-    // Declare a variable var_char of type character and set its value to a.
+    // Declare a variable var_char of type character and set its value to A.
     //return var_char;
 }
 
